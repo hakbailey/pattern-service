338a61ff049a1c47d3524ed81ab7e432d418aa26
Alina Buzachis <abuzachis@redhat.com>
2025-07-01 17:06:48 +0200

    Update
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -1,5 +1,3 @@
-from unittest.mock import patch
-
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -9,7 +7,6 @@ from core.models import ControllerLabel
 from core.models import Pattern
 from core.models import PatternInstance
 from core.models import Task
-from core.tasks import run_pattern_task
 
 
 class SharedDataMixin:
@@ -325,3 +322,86 @@ class TaskViewSetTest(SharedDataMixin, APITestCase):
         url = reverse("task-detail", args=[99999])
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+<<<<<<< HEAD
+=======
+
+
+class PatternViewSetTest(SharedDataMixin, APITestCase):
+    def test_pattern_list_view(self):
+        url = reverse("pattern-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["pattern_name"], "example_pattern")
+
+    def test_pattern_detail_view(self):
+        url = reverse("pattern-detail", args=[self.pattern.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["collection_name"], "mynamespace.mycollection")
+
+    @patch("core.views.async_to_sync")
+    def test_pattern_create_view(self, mock_async_to_sync):
+        url = reverse("pattern-list")
+        data = {
+            "collection_name": "new.namespace.collection",
+            "collection_version": "1.2.3",
+            "collection_version_uri": "https://example.com/new.tar.gz",
+            "pattern_name": "new_pattern",
+        }
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+
+        # Pattern created
+        pattern = Pattern.objects.get(pattern_name="new_pattern")
+        self.assertIsNotNone(pattern)
+
+        # Task id returned directly
+        task_id = response.data.get("task_id")
+        self.assertIsInstance(task_id, int)
+
+        # Task exists
+        task = Task.objects.get(id=task_id)
+        self.assertEqual(task.status, "Initiated")
+        self.assertEqual(task.details.get("model"), "Pattern")
+        self.assertEqual(task.details.get("id"), pattern.id)
+
+
+class PatternInstanceViewSetTest(SharedDataMixin, APITestCase):
+    def test_pattern_instance_list_view(self):
+        url = reverse("patterninstance-list")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_pattern_instance_detail_view(self):
+        url = reverse("patterninstance-detail", args=[self.pattern_instance.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["organization_id"], 1)
+
+    def test_pattern_instance_create_view(self):
+        url = reverse("patterninstance-list")
+        data = {
+            "organization_id": 2,
+            "controller_project_id": 0,
+            "controller_ee_id": 0,
+            "credentials": {"user": "tester"},
+            "executors": [],
+            "pattern": self.pattern.id,
+        }
+
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        instance = PatternInstance.objects.get(organization_id=2)
+        self.assertIsNotNone(instance)
+
+        task_id = response.data.get("task_id")
+        self.assertIsInstance(task_id, int)
+
+        task = Task.objects.get(id=task_id)
+        self.assertEqual(task.status, "Initiated")
+        self.assertEqual(task.details.get("model"), "PatternInstance")
+        self.assertEqual(task.details.get("id"), instance.id)
+>>>>>>> fbc5163 (Update)
--- core/views.py
+++ core/views.py
@@ -1,7 +1,6 @@
 import uuid
 
 from ansible_base.lib.utils.views.ansible_base import AnsibleBaseView
-from asgiref.sync import async_to_sync
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.request import Request
@@ -41,9 +40,6 @@ class PatternViewSet(CoreViewSet, ModelViewSet):
             status="Initiated", details={"model": "Pattern", "id": pattern.id}
         )
 
-        async_to_sync(run_pattern_task)(pattern.id, task.id)
-
-        headers = self.get_success_headers(serializer.data)
         return Response(
             {
                 "task_id": task.id,
@@ -52,7 +48,6 @@ class PatternViewSet(CoreViewSet, ModelViewSet):
                 ),
             },
             status=status.HTTP_202_ACCEPTED,
-            headers=headers,
         )
 
 
