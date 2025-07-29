cb444813f4844c30e47efc166f1dfa002317e2df
Alina Buzachis <abuzachis@redhat.com>
2025-07-29 17:07:12 +0200

    Rebase
    Signed-off-by: Alina Buzachis <abuzachis@redhat.com>

--- core/tests/test_controller_client.py
+++ core/tests/test_controller_client.py
@@ -1,12 +1,10 @@
-from core.controller_client import _aap_session
-from core.controller_client import get_http_session
-
+import core.controller_client as cc
 
 def test_get_http_session_caches():
     """Subsequent calls without force_refresh must return the *same* object."""
-    s1 = get_http_session()
-    s2 = get_http_session()
+    s1 = cc.get_http_session()
+    s2 = cc.get_http_session()
     assert s1 is s2
 
-    s3 = get_http_session(force_refresh=True)
-    assert s3 is not s1 and s3 is _aap_session
+    s3 = cc.get_http_session(force_refresh=True)
+    assert s3 is not s1 and s3 is cc._aap_session
